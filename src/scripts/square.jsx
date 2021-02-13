577c1de705edd6683425123ce546b95ccca2bab5
Ryan <ryan@Ryans-MacBook-Pro.local>
2021-02-12 20:18:22 -0500

    initial icon added

--- src/scripts/square.jsx
+++ src/scripts/square.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 
+
 export default class Square extends Component {
     constructor(props) {
       super(props);
@@ -19,15 +20,20 @@ export default class Square extends Component {
     
     render() {
       let classname = '';
+      let cls = '';
       if (this.props.start) {
-        classname = ' square-start'
+        classname = ' square-start';
+        cls = 'glyphicon glyphicon-play';
       } else if (this.props.end) {
-        classname = ' square-end'
+        classname = ' square-end stop'
+        cls = 'glyphicon glyphicon-stop'
       } else if (this.props.wall) {
         classname = ' square-wall'
+        cls = 'glyphicon glyphicon-hi'
       } else {}
       return (
           <div disabled={this.props.disabled} id={String(this.props.row) + "-" + String(this.props.col)} className={`square` + classname} onMouseDown={() => this.props.handleDown(this.props.row, this.props.col)} onMouseUp={() => this.props.handleUp()} onMouseMove={() => this.props.handleMove(this.props.row, this.props.col)}>
+          <span className={cls}></span>
           </div>
       )  
       
