b76dd0a83a7f831e3253c4b64e2ccb7aab83b230
Ryan Berg <ryanmcberg@gmail.com>
2021-07-19 14:00:18 -0400

    Updated maze to be animated, not replace entire board, but juse maze nodes.

--- src/scripts/maze/maze.js
+++ src/scripts/maze/maze.js
@@ -10,42 +10,58 @@ class Maze {
     }
 
     getMaze() {
-        return this.maze.getMaze();
+        return this.maze.getWallOrder();
     }
 
 }
 
+
+// Generates a Horizontal or Vertically split maze depending on the original input choice.
 class HorizontalVerticalMaze {
     constructor(grid, type) {
+        this.searched = [];
         this.grid = grid.slice();
         this.gridWidth = grid[0].length;
         this.gridHeight = grid.length;
-        this.mazify(0, this.gridWidth, 0,  this.gridHeight, type);
+        this.addTopBottomSides();
+        this.mazify(1, this.gridWidth-1, 1,  this.gridHeight-1, type);
     }
 
-    getMaze() {
-        return this.grid;
+    getWallOrder() {
+        return this.searched;
+    }
+
+    //Makes walls on top, bottom, and sides
+    addTopBottomSides() {
+        for(let i = 0; i < this.gridWidth; i++) {
+            this.searched.push(this.grid[0][i]);
+            this.searched.push(this.grid[this.gridHeight-1][i]);
+        }
+        for(let i = 0; i < this.gridHeight; i++) {
+            this.searched.push(this.grid[i][0]);
+            this.searched.push(this.grid[i][this.gridWidth - 1]);
+        }
     }
 
     mazify(lowWidth, highWidth, lowHeight, highHeight, type) {
         if (type === "vertical") {
             let mid = Math.floor((lowWidth + highWidth)/2)
             if ((highWidth - lowWidth) > 2) {
-                for(let i = lowHeight; i < highHeight; i++) {
-                    let curNode = this.grid[i][mid];
-                    if(!curNode.start && !curNode.end) {
-                        curNode.wall = true;
-                    } 
-                }
 
                 let midHeight = Math.floor((lowHeight+highHeight)/2);
                 let topRandom = Math.floor(Math.random() * (midHeight - lowHeight)) + lowHeight;
                 let botRandom = Math.floor(Math.random() * (highHeight - midHeight)) + midHeight; 
-                this.grid[topRandom][mid].wall = false;
-                this.grid[botRandom][mid].wall = false;
+
+                for(let i = lowHeight; i < highHeight; i++) {
+                    let curNode = this.grid[i][mid];
+                    if(!curNode.start && !curNode.end && i != topRandom && i != botRandom) {
+                        this.searched.push(curNode);
+                    } 
+                }
 
                 this.mazify(lowWidth, mid, lowHeight, highHeight, "horizontal");
                 this.mazify(mid+1, highWidth, lowHeight, highHeight, "horizontal");
+
             }
             
         }
@@ -53,18 +69,17 @@ class HorizontalVerticalMaze {
         if (type === "horizontal") {
             let mid = Math.floor((lowHeight + highHeight)/2);
             if((highHeight - lowHeight) > 2) {
-                for(let i = lowWidth; i < highWidth; i++) {
-                    let curNode = this.grid[mid][i];
-                    if(!curNode.start && !curNode.end) {
-                        curNode.wall = true;
-                    } 
-                }
 
                 let midWidth = Math.floor((lowWidth+highWidth)/2);
                 let topRandom = Math.floor(Math.random() * (midWidth - lowWidth)) + lowWidth;
                 let botRandom = Math.floor(Math.random() * (highWidth - midWidth)) + midWidth; 
-                this.grid[mid][topRandom].wall = false;
-                this.grid[mid][botRandom].wall = false;
+
+                for(let i = lowWidth; i < highWidth; i++) {
+                    let curNode = this.grid[mid][i];
+                    if(!curNode.start && !curNode.end && i != topRandom && i != botRandom) {
+                        this.searched.push(curNode);
+                    } 
+                }
 
                 this.mazify(lowWidth, highWidth, lowHeight, mid, "vertical");
                 this.mazify(lowWidth, highWidth, mid+1, highHeight, "vertical");
@@ -74,17 +89,31 @@ class HorizontalVerticalMaze {
     }
 }
 
+// Generates a random horizontally split maze.
 class RandomMaze {
 
     constructor(grid) {
+        this.searched = [];
         this.grid = grid.slice();
         this.gridWidth = grid[0].length;
         this.gridHeight = grid.length;
-        this.mazify(0, this.gridWidth, 0,  this.gridHeight, "horizontal");
+        this.addTopBottomSides();
+        this.mazify(1, this.gridWidth-1, 1,  this.gridHeight-1, "horizontal");
     }
 
-    getMaze() {
-        return this.grid;
+    getWallOrder() {
+        return this.searched;
+    }
+
+    addTopBottomSides() {
+        for(let i = 0; i < this.gridWidth; i++) {
+            this.searched.push(this.grid[0][i]);
+            this.searched.push(this.grid[this.gridHeight-1][i]);
+        }
+        for(let i = 0; i < this.gridHeight; i++) {
+            this.searched.push(this.grid[i][0]);
+            this.searched.push(this.grid[i][this.gridWidth - 1]);
+        }
     }
 
     mazify(lowWidth, highWidth, lowHeight, highHeight, type) {
@@ -94,10 +123,8 @@ class RandomMaze {
                 for(let i = lowHeight; i < highHeight; i++) {
                     let curNode = this.grid[i][mid];
                     let chance = Math.random();
-                    if(!curNode.start && !curNode.end && chance <= .50) {
-                        curNode.wall = true;
-                    } else if(!curNode.start && !curNode.end) {
-                        curNode.wall = false;
+                    if(!curNode.start && !curNode.end && chance <= .55) {
+                        this.searched.push(curNode);
                     }
                 }
                 this.mazify(lowWidth, mid, lowHeight, highHeight, "horizontal");
@@ -112,10 +139,8 @@ class RandomMaze {
                 for(let i = lowWidth; i < highWidth; i++) {
                     let curNode = this.grid[mid][i];
                     let chance = Math.random();
-                    if(!curNode.start && !curNode.end && chance < .50) {
-                        curNode.wall = true;
-                    } else if(!curNode.start && !curNode.end) {
-                        curNode.wall = false;
+                    if(!curNode.start && !curNode.end && chance < .55) {
+                        this.searched.push(curNode);
                     }
                 }
                 this.mazify(lowWidth, highWidth, lowHeight, mid, "vertical");
